4efe6b1097be700da0837ccf58bdc06c3f4cf3cf
sbalint98 <sbsbalint14@gmail.com>
2020-04-24 01:41:32 +0200

    Cherry picked the relevant commits for printf

--- include/hip/hcc_detail/device_functions.h
+++ include/hip/hcc_detail/device_functions.h
@@ -33,6 +33,21 @@ THE SOFTWARE.
 #include <hip/hip_vector_types.h>
 #include <hip/hcc_detail/device_library_decls.h>
 #include <hip/hcc_detail/llvm_intrinsics.h>
+
+#if __HIP_CLANG_ONLY__ && __HIP_VDI__
+extern "C" __device__ int printf(const char *fmt, ...);
+#else
+#if HC_FEATURE_PRINTF
+template <typename... All>
+static inline __device__ void printf(const char* format, All... all) {
+    hc::printf(format, all...);
+}
+#else
+template <typename... All>
+static inline __device__ void printf(const char* format, All... all) {}
+#endif // HC_FEATURE_PRINTF
+#endif // __HIP_CLANG_ONLY__ && __HIP_VDI__
+
 /*
 Integer Intrinsics
 */
--- include/hip/hcc_detail/hip_runtime.h
+++ include/hip/hcc_detail/hip_runtime.h
@@ -311,16 +311,6 @@ extern "C" __device__ void* __hip_free(void* ptr);
 static inline __device__ void* malloc(size_t size) { return __hip_malloc(size); }
 static inline __device__ void* free(void* ptr) { return __hip_free(ptr); }
 
-#if defined(__HCC_ACCELERATOR__) && defined(HC_FEATURE_PRINTF)
-template <typename... All>
-static inline __device__ void printf(const char* format, All... all) {
-    hc::printf(format, all...);
-}
-#elif defined(__HCC_ACCELERATOR__) || __HIP__
-template <typename... All>
-static inline __device__ void printf(const char* format, All... all) {}
-#endif
-
 #endif //__HCC_OR_HIP_CLANG__
 
 #ifdef __HCC__
